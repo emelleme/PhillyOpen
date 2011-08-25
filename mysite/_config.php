e6c1ecd7bf071f7746fb869ce42753eaba27862a
Wittney Bolden <wbolden@programmer.net>
2011-08-25 19:38:01 -0400

    Created reference to individual database settings so that each user can customize.  Added forwarding capabilities for mobile browsers

--- mysite/_config.php
+++ mysite/_config.php
@@ -1,18 +1,23 @@
 <?php
+include(BASE_PATH.DIRECTORY_SEPARATOR."mysite".DIRECTORY_SEPARATOR."database_information.php");
 
 global $project;
 $project = 'mysite';
 
-global $database;
-$database = '';
-
-require_once('conf/ConfigureFromEnv.php');
 
 MySQLDatabase::set_connection_charset('utf8');
 
+
 // This line set's the current theme. More themes can be
 // downloaded from http://www.silverstripe.org/themes/
-SSViewer::set_theme('blackcandy');
+
+if ($browser->isMobile())
+{
+	Director::redirect('http://twitter.com/#!/PhillyOpen');
+}
+
+// Set the site locale
+i18n::set_locale('en_US');
 
 // enable nested URLs for this site (e.g. page/sub-page/)
 SiteTree::enable_nested_urls();
